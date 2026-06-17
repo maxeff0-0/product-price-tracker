3cfa67b8a5b6035058b45a3a1327821978f005ee
maxeff0-0 <maxeff.dev@gmail.com>
2026-06-17 16:51:41 +0530

    Switch back to headless feature

--- src/tracker.py
+++ src/tracker.py
@@ -2,17 +2,14 @@ from playwright.sync_api import sync_playwright
 
 pw = sync_playwright().start()
 
-browser = pw.chromium.launch(
-    headless=False,
-    slow_mo=2000
-)
+browser = pw.chromium.launch()
 page = browser.new_page()
 user_url = input("Enter product url(amazon): ")
 str1 = user_url.split("dp/")
 str2 = str1[1].split("/")[0] + "/"
 product_url = str1[0] + "dp/" + str2
-page.goto(product_url)
 print("\nFetching product details...")
+page.goto(product_url)
 
 product_title = page.locator("#productTitle").text_content().strip()
 product_price = page.locator(".a-price-symbol").first.text_content().strip() + page.locator(".a-price-whole").first.text_content().strip()
