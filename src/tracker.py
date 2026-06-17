c943d88cb6a3be4097c4d4591e11b9bc8c7ba963
maxeff0-0 <maxeff.dev@gmail.com>
2026-06-17 16:33:39 +0530

    Add amazon product title and price extraction

--- src/tracker.py
+++ src/tracker.py
@@ -12,4 +12,12 @@ str1 = user_url.split("dp/")
 str2 = str1[1].split("/")[0] + "/"
 product_url = str1[0] + "dp/" + str2
 page.goto(product_url)
+print("\nFetching product details...")
+
+product_title = page.locator("#productTitle").text_content().strip()
+product_price = page.locator(".a-price-symbol").first.text_content().strip() + page.locator(".a-price-whole").first.text_content().strip()
+
+print("\nProduct title: " + product_title)
+print("Product price: " + product_price)
+
 browser.close()
